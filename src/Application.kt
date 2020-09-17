b3ef9e9ed1981ed3e955721b8e78db0bc06db700
zl <zl@astarschool.com>
2020-09-17 18:12:16 +0800

    add velocity

--- src/Application.kt
+++ src/Application.kt
@@ -1,16 +1,25 @@
 package io.github.pdkst
 
+import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapter
+import com.google.gson.TypeAdapterFactory
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
-import io.ktor.response.*
-import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.request.*
-import java.net.URI
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.velocity.*
+import org.apache.velocity.runtime.RuntimeConstants
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.text.DateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
+val gson = GsonBuilder().disableHtmlEscaping().setPrettyPrinting().setDateFormat(DateFormat.LONG).create()
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
@@ -25,6 +34,15 @@ fun Application.module(testing: Boolean = false) {
             }
         }
         install(CORS)
+        install(Velocity) {
+            //模板引擎参数实例化
+            val ep = Properties()
+            //指定资源的加载类型
+            ep.setProperty(RuntimeConstants.RESOURCE_LOADER, "file,classpath")
+            //允许#set设置null
+            ep.setProperty("directive.set.null.allowed", "true")
+            init(ep)
+        }
         static {
             defaultResource("index.html", "web")
             // /resource/web/ 下面的所有文件
@@ -58,6 +76,20 @@ fun Application.module(testing: Boolean = false) {
         //http://localhost:8080/redirect
         get("/redirect") {
             call.respondRedirect("http://www.baidu.com", permanent = true)
+        }
+
+        get("/vm/{vmName}") {
+            val vmName = call.parameters["vmName"]?.trim()?.let { call.parameters["vmName"] } ?: "test.vm"
+            val content = Files.newBufferedReader(Paths.get("resources/vm/$vmName.json"))
+            val model = gson.fromJson(content, hashMapOf<String, Any>().javaClass)
+            call.respond(VelocityContent("resources/vm/$vmName.vm", model))
+        }
+
+        get("/root/{vmName}") {
+            val vmName = call.parameters["vmName"]?.trim()?.let { call.parameters["vmName"] } ?: "test.vm"
+            val content = Files.newBufferedReader(Paths.get("$vmName.json"))
+            val model = gson.fromJson(content, hashMapOf<String, Any>().javaClass)
+            call.respond(VelocityContent("$vmName.vm", model))
         }
     }
 }
